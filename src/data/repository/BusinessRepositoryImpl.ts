a52be6a208c6ab092b7f01697468e91f56cc33aa
RaulPalade <raulpalade@outlook.com>
2024-04-28 13:45:38 +0200

    [ADD] BusinessRepo implementation

--- src/data/repository/BusinessRepositoryImpl.ts
+++ src/data/repository/BusinessRepositoryImpl.ts
@@ -1,6 +1,9 @@
+import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 import { Business, NewBusiness, Result } from "../../domain/models";
+import { Success, Failure } from "../../domain/models/Result";
 import { BusinessRepository } from "../../domain/repository/BusinessRepository";
 import { BusinessDataSource } from "../datasource/BusinessDataSource";
+import { ResponseSuccess } from "../models/ApiResponse";
 
 export class BusinessRepositoryImpl implements BusinessRepository {
   dataSource: BusinessDataSource;
@@ -8,19 +11,95 @@ export class BusinessRepositoryImpl implements BusinessRepository {
     this.dataSource = dataSource;
   }
 
+  async getAllBusinesses(): Promise<Result<Business[]>> {
+    const businesses: Business[] = [];
+    const businessesResponse = await this.dataSource.getAllBusinesses();
+    if (businessesResponse instanceof ResponseSuccess) {
+      const snapshot: QueryDocumentSnapshot<DocumentData, DocumentData>[] =
+        businessesResponse.value;
+      snapshot.forEach((doc) => {
+        const businessData = doc.data();
+        const business: Business = {
+          id: doc.id,
+          name: businessData.name,
+          street: businessData.street,
+          city: businessData.city,
+          website: businessData.website,
+          reviews: businessData.reviews,
+          score: businessData.score,
+          phone: businessData.phone,
+          category: businessData.category,
+          urlMap: businessData.urlMap,
+        };
+        businesses.push(business);
+      });
+      return new Success(businesses);
+    } else {
+      const error: string = (businessesResponse as Failure<string>).error;
+      return new Response(error);
+    }
+  }
+
   async addBusinesses(businesses: NewBusiness[]): Promise<Result<boolean>> {
-    throw new Error("Method not implemented.");
+    try {
+      const promises: Promise<Result<boolean>>[] = [];
+      for (const business of businesses) {
+        promises.push(
+          new Promise<Result<boolean>>(async (resolve, reject) => {
+            try {
+              const businessResponse = await this.dataSource.addBusiness(
+                business
+              );
+              if (businessResponse instanceof ResponseSuccess) {
+                resolve(new Success(true));
+              } else {
+                const error: string = (businessResponse as Failure<string>)
+                  .error;
+                reject(
+                  new Failure(
+                    `Failed to add business: ${error}. Business: ${JSON.stringify(
+                      business
+                    )}`
+                  )
+                );
+              }
+            } catch (error) {
+              reject(
+                new Failure(
+                  `Failed to add business: ${error}. Business: ${JSON.stringify(
+                    business
+                  )}`
+                )
+              );
+            }
+          })
+        );
+      }
+
+      await Promise.all(promises);
+      return new Success(true);
+    } catch (error) {
+      return new Failure(error);
+    }
   }
 
   async deleteBusiness(businessId: string): Promise<Result<boolean>> {
-    throw new Error("Method not implemented.");
+    const deleteResponse = await this.dataSource.deleteBusiness(businessId);
+    if (deleteResponse instanceof ResponseSuccess) {
+      return new Success(true);
+    } else {
+      const error: string = (deleteResponse as Failure<string>).error;
+      return new Failure(error);
+    }
   }
 
   async updateBusiness(business: Business): Promise<Result<boolean>> {
-    throw new Error("Method not implemented.");
-  }
-
-  async getAllBusinesses(): Promise<Result<Business[]>> {
-    throw new Error("Method not implemented.");
+    const updateResponse = await this.dataSource.updateBusiness(business);
+    if (updateResponse instanceof ResponseSuccess) {
+      return new Success(true);
+    } else {
+      const error: string = (updateResponse as Failure<string>).error;
+      return new Failure(error);
+    }
   }
 }
